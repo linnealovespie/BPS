bfec0bea88e5ca3c127d3c0f285230d254edb801
BenBagBag <isaac.cowhey@gmail.com>
2023-04-17 17:11:11 -0700

    make utils a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='utils',
+version='0.1',
+description='An example package',
+url='#',
+author='',
+install_requires=['numpy', 'pandas', 'shapely', 'geopandas', 'matplotlib'],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
